7007416dd88110f158bfd389190b5c5a196333cf
Степан Климов <stepan.chik228@mail.ru>
2025-09-25 21:14:40 +0500

    Разработанный класс Channel. Климов

---
+++ TV_Klimov/Classes/Channel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TV_Klimov.Classes
+{
+    internal class Channel
+    {
+    }
+}
